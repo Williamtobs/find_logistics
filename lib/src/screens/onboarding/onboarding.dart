1796613e4aa2ee7d97aa991359bc21f4f01cb01f
William <akeemtobi6@gmail.com>
2022-12-05 20:56:40 -0800

    New onboard

--- lib/src/screens/onboarding/onboarding.dart
+++ lib/src/screens/onboarding/onboarding.dart
@@ -21,6 +21,25 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     return Scaffold(
       body: Column(
         children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const UserSelectMode()));
+              },
+              child: Text('SKIP',
+                  style: GoogleFonts.poppins(
+                      color: primaryColor,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500)),
+            ),
+          ),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
@@ -31,47 +50,49 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                     _index = index;
                   });
                 },
-                children: const [
+                children: [
                   EachSlide(
-                      description:
-                          'The Quickest and Easy way to hire a dispatch rider',
-                      title: 'Quick and Easy',
-                      image: 'assets/images/slide1.svg'),
+                    description:
+                        'The Quickest and Easy way to hire a dispatch rider',
+                    title: 'Quick and Easy',
+                    image: 'assets/images/slide1.svg',
+                    index: _index,
+                  ),
                   EachSlide(
-                      description:
-                          'Deliver now or schedule a delivery for later',
-                      title: 'Easy Delivery',
-                      image: 'assets/images/slide2.svg'),
+                    description: 'Deliver now or schedule a delivery for later',
+                    title: 'Easy Delivery',
+                    image: 'assets/images/slide2.svg',
+                    index: _index,
+                  ),
                   EachSlide(
-                      description:
-                          'The Quickest and Easy way to hire a dispatch rider',
-                      title: 'Quick and Easy',
-                      image: 'assets/images/slide3.svg'),
+                    description:
+                        'The Quickest and Easy way to hire a dispatch rider',
+                    title: 'Quick and Easy',
+                    image: 'assets/images/slide3.svg',
+                    index: _index,
+                  ),
                 ],
               ),
             ),
           ),
-          const SizedBox(
-            height: 10,
-          ),
-          AnimatedSmoothIndicator(
-            activeIndex: _index,
-            count: 3,
-            effect: ExpandingDotsEffect(
-              dotHeight: 10,
-              dotWidth: 10,
-              spacing: 5,
-              activeDotColor: primaryColor,
-              dotColor: secondaryColor,
-            ),
-          ),
+          // const SizedBox(
+          //   height: 10,
+          // ),
+          // AnimatedSmoothIndicator(
+          //   activeIndex: _index,
+          //   count: 3,
+          //   effect: ExpandingDotsEffect(
+          //     dotHeight: 10,
+          //     dotWidth: 10,
+          //     spacing: 5,
+          //     activeDotColor: primaryColor,
+          //     dotColor: secondaryColor,
+          //   ),
+          // ),
           const SizedBox(
             height: 30,
           ),
-          const Divider(
-            color: Colors.grey,
-            thickness: 1,
-          ),
+
           Padding(
             padding: const EdgeInsets.symmetric(
               horizontal: 20,
@@ -121,11 +142,13 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
 
 class EachSlide extends StatelessWidget {
   final String title, description, image;
+  final int index;
   const EachSlide(
       {Key? key,
       required this.title,
       required this.description,
-      required this.image})
+      required this.image,
+      required this.index})
       : super(key: key);
 
   @override
@@ -133,12 +156,24 @@ class EachSlide extends StatelessWidget {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        const SizedBox(height: 30),
         SvgPicture.asset(image, height: 300),
-        const SizedBox(height: 60),
+        const SizedBox(height: 40),
+        AnimatedSmoothIndicator(
+          activeIndex: index,
+          count: 3,
+          effect: ExpandingDotsEffect(
+            dotHeight: 10,
+            dotWidth: 10,
+            spacing: 5,
+            activeDotColor: primaryColor,
+            dotColor: secondaryColor,
+          ),
+        ),
         Text(
           title,
           style: GoogleFonts.inter(
-            fontSize: 20,
+            fontSize: 24,
             fontWeight: FontWeight.w700,
           ),
         ),
@@ -147,7 +182,7 @@ class EachSlide extends StatelessWidget {
           description,
           textAlign: TextAlign.center,
           style: GoogleFonts.inter(
-            fontSize: 14,
+            fontSize: 13,
             fontWeight: FontWeight.w400,
           ),
         ),
